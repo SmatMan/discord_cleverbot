40bf07f2a4c938112f88d34d7f6f4f76d0e81e6d
SmatMan <gamingajun10@icloud.com>
2021-04-04 10:54:21 -0400

    add a command where the bot can talk to itself

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import asyncio
 import cleverbotfree.cbfree
 import time
 import sys
+import re
 
 import config as cfg # config.py file
 
@@ -45,5 +46,45 @@ async def start(ctx):
 async def start_error(ctx, error):
     if isinstance(error, discord.ext.commands.errors.CommandOnCooldown):
         await ctx.send(f"Hey, <@{ctx.message.author.id}>! Someone is already using the bot.")
+
+@bot.command()
+@cooldown(1, 100000)
+async def talktoself(ctx):
+    await ctx.send("Starting up...")
+    cb = cleverbotfree.cbfree.Cleverbot()
+    print(f"{str(ctx.message.author)} has started a talk to self session.")
+    try:
+        cb.browser.get(cb.url)
+        welcomeEmbed = discord.Embed(title="Welcome to the Talk to Self command!", description="this just makes the bot talk to itself over and over lmao have fun idk. it stops at 100 messages.", color=0x00D700)
+        await ctx.send(embed=welcomeEmbed)
+        messageCounter = 0
+                        
+        await ctx.send("**Send a topic for the bot to start talking about.**")
+        cbInput = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
+        cb.get_form()
+        cb.send_input(str(cbInput.content))
+        botResponse = cb.get_response()
+        await ctx.send(botResponse)
+        while True:
+            if messageCounter <= 100:
+                cb.get_form()
+                async with ctx.typing():
+                    cb.send_input(str(botResponse))
+                    botResponse = cb.get_response()
+                await ctx.send(botResponse)
+                messageCounter += 1
+
+        cb.browser.close()
+        start.reset_cooldown(ctx)
+        stopEmbed = discord.Embed(title="Session Ended.", description="Bye!", color=0xFF0000)
+        await ctx.send(embed=stopEmbed)
+    except KeyboardInterrupt:
+        cb.browser.close()
+        start.reset_cooldown(ctx)
+@talktoself.error
+async def talktoself_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.CommandOnCooldown):
+        await ctx.send(f"Hey, <@{ctx.message.author.id}>! Someone is already using this command.")
+
 
 bot.run(cfg.token)
